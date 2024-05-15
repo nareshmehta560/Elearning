e7622374e4088f490c4b5acee320d7ceccc51c5c
nareshkumarmehta <naresh.kumar.mehta@stud.th-luebeck.de>
2024-05-15 17:30:39 +0200

    #6-register merge with main

--- src/main/java/com/swt_II/elearningplatform/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swt_II.elearningplatform.Repository;
-
-import com.swt_II.elearningplatform.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
