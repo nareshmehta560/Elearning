c13235a51a213c2952d67d028f225fcab673aa85
nareshkumarmehta <naresh.kumar.mehta@stud.th-luebeck.de>
2024-05-20 19:53:55 +0200

    Test added

--- src/test/java/com/swt_II/elearningplatform/InstructorTest.java
+++ src/test/java/com/swt_II/elearningplatform/InstructorTest.java
@@ -1,50 +1,69 @@
 package com.swt_II.elearningplatform;
-
+import com.swt_II.elearningplatform.model.role.Role;
+import com.swt_II.elearningplatform.model.user.Instructor;
+import com.swt_II.elearningplatform.model.user.InstructorService;
+import com.swt_II.elearningplatform.model.role.RoleService;
+import com.swt_II.elearningplatform.model.user.User;
+import com.swt_II.elearningplatform.repositories.InstructorRepository;
+import com.swt_II.elearningplatform.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
 public class InstructorTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @InjectMocks
+    private InstructorService instructorService;
 
-    @Autowired
-    private WebApplicationContext context;
+    @Mock
+    private InstructorRepository instructorRepository;
 
+    @Mock
+    private RoleService roleService;
 
+    @Mock
+    private UserRepository userRepository;
 
     @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .build();
+    public void init() {
+        MockitoAnnotations.initMocks(this);
     }
-    @Test
-    @WithUserDetails("testUser2")
-    public void givenNonAdminUser_whenGetNewInstructors_thenForbidden() throws Exception {
-        mockMvc.perform(get("/newInstructors"))
-                .andExpect(status().isForbidden());
-    }
+
 
     @Test
-    @WithUserDetails("testUser")
-    public void givenAdminUser_whenGetNewInstructors_thenOk() throws Exception {
-        mockMvc.perform(get("/newInstructors"))
-                .andExpect(status().isOk());
+    public void testApproveInstructor() {
+        User user = new User();
+        user.setUserName("testUser2");
+        user.setPassWord("$2a$12$yI8wA5Kd0pcFknDHqyZgN.9/wAHmVzLRCpsBNuUiCSLazDJ4tYM8u");
+        user.setEmail("test@gmail.com");
+        user.addRole(roleService.getRoleFromRoleName("USER"));
+        when(userRepository.findByUserName("testUser2")).thenReturn(user);
+
+
+        Long id = 1L;
+        Instructor instructor = new Instructor("MSc","5 years");
+        instructor.setPaypalEmail("test@gmail.com");
+        User user1 = userRepository.findByUserName("testUser2");
+        instructor.setUser(user1);
+
+        when(instructorRepository.findById(id)).thenReturn(Optional.of(instructor));
+
+        instructorService.approveInstructor(id);
+        assertTrue(user.getRoles().contains(roleService.getRoleFromRoleName("INSTRUCTOR")));
+        assertTrue(instructor.isApproved()); // Check if the instructor is approved
+
+        verify(instructorRepository, times(1)).findById(id);
+        verify(instructorRepository, times(1)).save(instructor);
+        verify(userRepository, times(1)).save(any());
     }
-
-
 }
