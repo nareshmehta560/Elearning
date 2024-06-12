939a8f008c22556035609f822359e7359cee2545
Ludwig, Marco <m.ludwig@stud.th-luebeck.de>
2024-06-12 22:26:10 +0200

    #27 added test

--- src/test/java/com/swt_II/elearningplatform/removeDashboardTest.java
+++ src/test/java/com/swt_II/elearningplatform/removeDashboardTest.java
@@ -1,11 +1,15 @@
 package com.swt_II.elearningplatform;
 
+import com.swt_II.elearningplatform.repositories.RoleRepository;
+import com.swt_II.elearningplatform.repositories.UserRepository;
 import jakarta.servlet.ServletContext;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.session.SessionProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -18,19 +22,23 @@ public class removeDashboardTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private RoleRepository roleRepo;
+
     @Test
     public void testDashboardUrl() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/dashboard"))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
     }
 
-    /*
+
     @Test
     public void testDashboardUrlLoggedIn() throws Exception {
-
-        mockMvc.perform(formLogin("/performLogin").user("testUser").password("usertest"));
-        mockMvc.perform(MockMvcRequestBuilders.get("/dashboard"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/dashboard")
+                        .with(SecurityMockMvcRequestPostProcessors.user("testUser")))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
-    */
 }
