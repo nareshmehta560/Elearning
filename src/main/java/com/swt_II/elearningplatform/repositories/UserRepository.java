3612714c5e560993774977b4341dba034a572577
Sashi <sashi.bhusan.yadav@stud.th-luebeck.de>
2024-06-20 11:37:16 +0200

    Revert "#17-wishlist_and_cart -> added and corrected the code to fetch the item in the cart from database and also to remove it"
    This reverts commit 9127172665eef3cb87e6d21a3d68a93697968069.

--- src/main/java/com/swt_II/elearningplatform/repositories/UserRepository.java
+++ src/main/java/com/swt_II/elearningplatform/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.swt_II.elearningplatform.repositories;
+
+import com.swt_II.elearningplatform.model.role.Role;
 import com.swt_II.elearningplatform.model.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User,Long> {
