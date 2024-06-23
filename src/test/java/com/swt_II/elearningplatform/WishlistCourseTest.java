a36876eed8f9879d51aeed7f65c8d98b9a3b1ea4
Bibek Upreti <bibekupreti1998@gmail.com>
2024-06-23 11:29:53 +0200

    #17-wishlist-and-cart test for duplicate items to wishlist geschrieben.

--- src/test/java/com/swt_II/elearningplatform/WishlistCourseTest.java
+++ src/test/java/com/swt_II/elearningplatform/WishlistCourseTest.java
@@ -60,5 +60,22 @@ public class WishlistCourseTest {
         verify(wishlistRepository, times(1)).save(wishlist);
         assertEquals(0, wishlist.getCourses().size());
     }
+    //test for duplicate items in wishlists
+    @Test
+    public void testAddCourseToWishlistDuplicate() {
+        User user = new User();
+        Course course = new Course();
+        course.setId(1L);
+        Wishlist wishlist = new Wishlist();
+        wishlist.setUser(user);
+        wishlist.getCourses().add(course);
+
+        when(wishlistRepository.findByUser(user)).thenReturn(wishlist);
+
+        wishlistService.addCourseToWishlist(user, course);
+
+        verify(wishlistRepository, times(1)).save(wishlist);
+        assertEquals(1, wishlist.getCourses().size());
+    }
 
 }
