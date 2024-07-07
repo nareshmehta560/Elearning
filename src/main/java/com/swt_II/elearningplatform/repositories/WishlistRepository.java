64fb0c1d10e5fd1d4be56ca3e1bca8ef1a73f18d
Sashi <sashi.bhusan.yadav@stud.th-luebeck.de>
2024-07-07 05:51:56 +0200

    #17-wishlist_and_cart -> added for wishlist

--- src/main/java/com/swt_II/elearningplatform/repositories/WishlistRepository.java
+++ src/main/java/com/swt_II/elearningplatform/repositories/WishlistRepository.java
@@ -1,11 +1,13 @@
 package com.swt_II.elearningplatform.repositories;
 
+import com.swt_II.elearningplatform.model.user.User;
 import com.swt_II.elearningplatform.model.wishlist.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
-    Wishlist findByUser(String username);
+    Wishlist findByUser(User user);
 
 }
+
